dceff8fd62350f0c7f6df2112e2554de65630dcf
manikpuri562 <manikpuri562@gmail.com>
2026-08-02 16:35:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ buttons.forEach(function(button) {
         alert("Opening blog post...");
     });
 });
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", function() {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        themeBtn.innerHTML = "☀️ Light Mode";
+    } else {
+        themeBtn.innerHTML = "🌙 Dark Mode";
+    }
+});
