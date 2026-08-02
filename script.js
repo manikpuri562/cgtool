6b5f050642615d33720688de78c7e1657fe3a100
manikpuri562 <manikpuri562@gmail.com>
2026-08-02 17:13:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,12 @@ search.addEventListener("keyup", function () {
         }
     });
 });
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    alert("✅ Thank you! Your message has been sent.");
+
+    contactForm.reset();
+});
