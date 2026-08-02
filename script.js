61652582ff0fbcbc7eaec131f0e1261c3f06442a
manikpuri562 <manikpuri562@gmail.com>
2026-08-02 17:30:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,15 +10,17 @@ buttons.forEach(function(button) {
 });
 const themeBtn = document.getElementById("themeBtn");
 
-themeBtn.addEventListener("click", function() {
-    document.body.classList.toggle("dark");
+if (themeBtn) {
+    themeBtn.addEventListener("click", function() {
+        document.body.classList.toggle("dark");
 
-    if (document.body.classList.contains("dark")) {
-        themeBtn.innerHTML = "☀️ Light Mode";
-    } else {
-        themeBtn.innerHTML = "🌙 Dark Mode";
-    }
-});
+        if (document.body.classList.contains("dark")) {
+            themeBtn.innerHTML = "☀️ Light Mode";
+        } else {
+            themeBtn.innerHTML = "🌙 Dark Mode";
+        }
+    });
+}
 const search = document.getElementById("search");
 const cards = document.querySelectorAll(".blog-card");
 
